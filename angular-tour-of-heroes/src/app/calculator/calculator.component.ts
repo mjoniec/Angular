5338b89971b642f2692a84479b91326b9a30c05c
mjoniec <mart1n.joniec@gmail.com>
2020-05-11 19:25:03 +0200

    binary calculator

--- angular-tour-of-heroes/src/app/calculator/calculator.component.ts
+++ angular-tour-of-heroes/src/app/calculator/calculator.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   counter = 0;
+  res = "";
+  btn0
 
   constructor() { }
 
@@ -16,5 +18,43 @@ export class CalculatorComponent implements OnInit {
 
   increment(){
     this.counter++;
+  }
+
+  btnEql_A(){
+    var a = this.res.split(/[-+*/]/);
+    var a0 = parseInt(a[0], 2);
+    var a1 = parseInt(a[1], 2);
+         if(this.res.includes("+")) this.res = (a0 + a1).toString(2);
+    else if(this.res.includes("-")) this.res = (a0 - a1).toString(2);
+    else if(this.res.includes("*")) this.res = (a0 * a1).toString(2);
+    else if(this.res.includes("/")) this.res = (a0 / a1).toString(2);
+  }
+
+  btn0_A() {
+    this.res = this.res + "0";
+  }  
+
+  btn1_A() {
+    this.res = this.res + "1";
+  }
+
+  btnClr_A() {
+    this.res = "";
+  } 
+
+  btnSum_A() {
+    this.res = this.res + "+";
+  }
+
+  btnSub_A() {
+    this.res = this.res + "-";
+  }  
+
+  btnMul_A() {
+    this.res = this.res + "*";
+  } 
+
+  btnDiv_A() {
+    this.res = this.res + "/";
   }
 }
