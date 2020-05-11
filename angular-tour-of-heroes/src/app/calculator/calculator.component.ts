9f008564850b34283f9e7fd63f5b8cb1c00adf5a
mjoniec <mart1n.joniec@gmail.com>
2020-05-11 18:41:03 +0200

    add clicker++

---
+++ angular-tour-of-heroes/src/app/calculator/calculator.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  counter = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  increment(){
+    this.counter++;
+  }
+}
